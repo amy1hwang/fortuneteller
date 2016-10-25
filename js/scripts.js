89fe8997363003205b32e42e06465251513dd6a1
Amy Hwang and Chris Clifford <amy@>
2016-10-25 14:04:05 -0700

    added final code to show a fortune after answering questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    $(".output").show();
+    $("#output").toggle();
 
     var qArray = [];
     $("input:checkbox[name=question]:checked").each(function() {
@@ -11,8 +11,23 @@ $(document).ready(function() {
 
     console.log(qArray);
 
+    var numUnlucky = 0;
+    var numLucky = 0;
 
-    $(".output").append(qArray.join(" "));
+    qArray.forEach(function(item) {
+      if (item === "lucky") {
+        numLucky += 1;
+      } else {
+        numUnlucky += 1;
+      }
+    });
+
+    if (numUnlucky <= numLucky) {
+      $("#fortune").text("You are a lucky person.  Good things are coming your way!");
+    } else {
+      $("#fortune").text("I'm sorry, but you are not lucky.");
+    }
+
 
   });
 })
